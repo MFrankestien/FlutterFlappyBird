b4f67dbb7350b4a6f8f2ef213767a78656d8c629
moooo.kh11@gmail.com <moooo.kh11@gmail.com>
2021-04-24 07:43:33 +0200

    main layout and assets

--- lib/main.dart
+++ lib/main.dart
@@ -17,17 +17,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-
-
-  final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
